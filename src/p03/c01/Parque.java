c605405f060ec33c07b2ed674f0b825578a0ede8
maugvb <slashera4@gmail.com>
2021-04-13 09:32:42 +0200

    Refactor method salirDelParque

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -45,7 +45,11 @@ public class Parque implements IParque{
 	// 
 	// TODO Método salirDelParque
 	//
-	
+	@Override
+	public void salirDelParque(String puerta) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	private void imprimirInfo (String puerta, String movimiento){
 		System.out.println(movimiento + " por puerta " + puerta);
@@ -86,11 +90,7 @@ public class Parque implements IParque{
 	}
 
 
-	@Override
-	public void salirDelParque(String puerta) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 
 }
